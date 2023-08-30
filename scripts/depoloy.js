74878aaa2b4e687d691ffea0fd61a005b0ba0612
onnanoco <moe@onnano.co>
2023-08-30 19:27:39 +0900

    Update depoloy.js

--- scripts/depoloy.js
+++ scripts/depoloy.js
@@ -2,7 +2,19 @@ require('dotenv').config();
 const hre = require('hardhat');
 
 async function main() {
+  const MoeErc20Wrapped = await hre.ethers.getContractFactory('MoeErc20Wrapped');
+  const moeTokenWrapped = await MoeErc20Wrapped.deploy();
 
+  await moeTokenWrapped.deployed();
+
+  console.log(`Wrapped MOE token contract has been deployed : ${moeTokenWrapped.address}`);
+
+  const MoeGovernor = await hre.ethers.getContractFactory('MoeGovernor');
+  const moeGovernor = await MoeGovernor.deploy();
+
+  await moeGovernor.deployed();
+
+  console.log(`MOE Governor contract has been deployed : ${moeGovernor.address}`);
 }
 
 main()
