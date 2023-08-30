2c5fc47ea7bbf0c03b1815abf1172759cce6fb91
onnanoco <moe@onnano.co>
2023-08-30 17:48:40 +0900

    initial commit

---
+++ scripts/depoloy.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const hre = require('hardhat');
+
+async function main() {
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
